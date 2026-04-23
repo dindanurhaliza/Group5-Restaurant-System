b20ffd0d68c55cb7a983a78d831f910a5ddffd22
Rusdi <rusdiadit210@gmail.com>
2026-04-23 13:52:33 +0700

    tambah class ke shared

---
+++ shared/MenuItem.java
@@ -0,0 +1,17 @@
+package shared;
+<<<<<<< HEAD
+public class MenuItem {
+    public int id;
+    public String name;
+    public String type; // makanan atau minuman
+    public double price;
+
+{
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.price = price;
+
+}
+}
+
