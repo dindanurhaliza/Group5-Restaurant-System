4e9fa735bec3049cb0628516ecd5274f694735d6
Rusdi <rusdiadit210@gmail.com>
2026-04-28 17:02:44 +0700

    update MenuManager

--- student1_rusdi/MenuManager.java
+++ student1_rusdi/MenuManager.java
@@ -1,41 +1,65 @@
 package student1_rusdi;
-import java.util.ArrayList;
+
 import shared.MenuItem;
 
-
-
 public class MenuManager {
-    private ArrayList<MenuItem> daftarMenu;
-    public MenuManager() {
-        this.daftarMenu = new ArrayList<>();
+    private MenuItem[] daftarMenu;
+    private int size;
+    private int capacity;
+public MenuManager() {
+    this.capacity = 2;
+    this.daftarMenu = new MenuItem[capacity];
+    this.size = 0;
+
+}
+
+private void grow(){
+    this.capacity = this.capacity*2;
+    MenuItem[] newArray = new MenuItem[capacity];
+
+    for (int i = 0; i < size; i++){
+        newArray[i] = daftarMenu[i];
+
+
+}
+
+this.daftarMenu = newArray;
+System.out.println("LOG: kapasitas mencapai batar. array diperluas " + capacity + "---");
+}
+
+
+public void tambahMenu(int id, String nama, double price) {
+    if (size == capacity){
+        grow();
     }
 
-    // fungsi utama s1 buat nsmbah menu
-    public void tambahMenu(int id, String nama, double price) {
-        MenuItem baru = new MenuItem(id, nama, "Makanan", price);
-        daftarMenu.add(baru);
-        System.out.println("berhasil input menu: " + nama);
+        MenuItem baru = new MenuItem(id, nama, "makanan", price);
+        daftarMenu[size] = baru;
+        size++;
+        System.out.println("berhasil input menu" + nama);
+
+    
+}
+
+public void tampilkanMenu(){
+    System.out.println("\n=== DAFTAR MENU ===");
+    for(int i = 0; i < size; i++){
+        MenuItem m = daftarMenu[i];
+        System.out.println("ID:" + m.id + " | "  + m.name + " | Harga: Rp" + m.price
+        );
+
     }
+}
 
-    // fungsi utama menampilkan daftar 
-    public void tampilkanMenu() {
-        System.out.println("\n=== REATORAN KEL 5 - DAFTAR MENU ===");
-        for (MenuItem m : daftarMenu) {
-            System.out.println("id:" + m.id + "| " + m.name +" | Harga: Rp" + m.price);
-        }
-    } 
+public static void main(String[] args) {
+    MenuManager app = new MenuManager();
+    app.tambahMenu(101, "nasi goreng", 20000);
+     app.tambahMenu(102, "ayam bakar", 25000);
+      app.tambahMenu(202, "es jeruk", 5000);
+       app.tambahMenu(203, "kopi", 6000);
+       app.tambahMenu(204, "cendol duren", 10000);
+       
+       app.tampilkanMenu();
+}
 
-    //main method langsung run 
-    public static void main (String[] args) {
-        MenuManager app = new MenuManager();
-
-        //input menu
-        app.tambahMenu(101, "Nasi goreng", 25000);
-        app.tambahMenu(102, "ayam bakar ", 32000);
-        app.tambahMenu(201, "es jeruk", 5000);
-        app.tambahMenu(113, "kopi", 8000);
-
-        // tampilkan hasil 
-        app.tampilkanMenu();
-    }
 }
